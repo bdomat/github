e270cd04eeea3bf1492bacd8c0257792c2556ecc
bdomat <bertrand.domat@gmail.com>
2023-01-13 22:35:53 +0100

    finalisation du projet

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
 
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
 // Ranger la sidebar si on click sur le contenu principal
 
 // BONUS
